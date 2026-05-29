6e8e3d77816d47a07214c99f8527900970e04bf6
karimisabah <167237974+karimisabah@users.noreply.github.com>
2026-05-29 07:57:02 +0200

    index

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Kraków Taximeter Service Worker
+const CACHE = "taximeter-v3";
+const ASSETS = ["./index.html", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {}));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request).then((r) => r || caches.match("./index.html")))
+  );
+});
